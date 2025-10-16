b46527fed56f82a7c87fc0160612607204548e8f
VRestini <vitor.restini@sptech.school>
2025-10-15 21:52:03 -0300

    fix: correcao do erro na leitura dos estados que estavam com falha.

--- leitura-arquivos-excel/src/main/java/we/travel/Main.java
+++ leitura-arquivos-excel/src/main/java/we/travel/Main.java
@@ -22,22 +22,22 @@ public class Main {
         arquivos.add("goias.xlsx" );
         arquivos.add("maranhao.xlsx" );
         arquivos.add("mato_grosso.xlsx" );
-        //arquivos.add("mato_grosso_do_sul.xlsx");
+        arquivos.add("mato_grosso_sul.xlsx");
         arquivos.add("minas_gerais.xlsx" );
         arquivos.add("para.xlsx" );
         arquivos.add("paraiba.xlsx");
         arquivos.add("parana.xlsx" );
         arquivos.add("pernambuco.xlsx" );
         arquivos.add("piaui.xlsx" );
-        //arquivos.add("rio_de_janeiro.xlsx" );
-        //arquivos.add("rio_grande_do_norte.xlsx" );
-        //arquivos.add("rio_grande_do_sul.xlsx");
+        arquivos.add("rio_janeiro.xlsx" );
+        arquivos.add("rio_grande_norte.xlsx" );
+        arquivos.add("rio_grande_sul.xlsx");
         arquivos.add("rondonia.xlsx" );
         arquivos.add("roraima.xlsx" );
         arquivos.add("santa_catarina.xlsx");
         arquivos.add("sao_paulo.xlsx" );
         arquivos.add("sergipe.xlsx" );
-        //arquivos.add("tocantins.xlsx" );
+        arquivos.add("tocantis.xlsx" );
 
 
         // Carregando o arquivo excel
